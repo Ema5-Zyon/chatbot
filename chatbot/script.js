cbf86f1806ddcde241956f619d30886ba2e22ff3
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-09 10:57:17 +0200

    modif in script.js to add message

--- chatbot/script.js
+++ chatbot/script.js
@@ -1,48 +1,50 @@
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
-        const send = document.getElementById("send");
+    const send = document.getElementById("send");
+
+
+
+    send.addEventListener("click", function (e) {
+
+        e.preventDefault();
+        ajax();
+    });
+    function ajax() {
+
+        if (window.XMLHttpRequest) {
+            var xhr = new XMLHttpRequest();
+        } else {
+            var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        let userMessage = document.getElementById("messages").value;
+
+        let param = "userMessages=" + userMessage;
 
         let messageDisplaySection = document.getElementById("messageDisplaySection");
 
-        send.addEventListener("click", function(e){
+        xhr.open("POST", 'bot.php', true);
 
-            e.preventDefault();
-            ajax();
-        });
-            function ajax(){
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-                if(window.XMLHttpRequest){
-                    var xhr = new XMLHttpRequest();
-                }else{
-                    var xhr = new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                
-                let userMessage = document.getElementById("messages").value;
+        xhr.send(param);
 
-                let param = "userMessages=" + userMessage;
-                
-                xhr.open("POST", 'bot.php', true );
-    
-                    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    
-                xhr.send(param);
-    
-                xhr.onreadystatechange = function(){
-        
-                    if( xhr.readyState == 4 && xhr.status == 200 ){
-    
-                        console.log(xhr.responseText);
-                        let obj = JSON.parse( xhr.responseText );
-                        console.log(obj);
-        
-                        messageDisplaySection.innerHTML = obj.resultat; 
-                        
-                    }
-                }
-                document.getElementById('messages').value = '';
+        xhr.onreadystatechange = function () {
+
+            if (xhr.readyState == 4 && xhr.status == 200) {
+
+                console.log(xhr.responseText);
+                let obj = JSON.parse(xhr.responseText);
+                console.log(obj);
+
+                messageDisplaySection.innerHTML += obj.resultat;
 
             }
-            
-       
+        }
+        document.getElementById('messages').value = '';
+
+    }
+
+
 
 })
